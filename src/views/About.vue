c87772bd46838450d883e59f3bee811a31eb9a28
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-04-25 16:32:35 -0700

    About in Japanese

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,26 @@
   <div class="max-w-lg mx-auto text-left p-2">
     <p class="text-3xl mb-2">About</p>
     <div class="space-y-2 mb-8">
+      <p>As film festivals, the Cannes Film Festival and the Academy Awards are well known.
+        However, these festivals are tied to old-fashioned business models, and we feel that a new type of film festival is needed.</p>
+      <p>There are many ways to convey information, including text, text + images, audio, audio + images, and video.
+        It is clear that video is the most efficient way to convey information.</p>
+      <p>Beyond that, I'm sure we'll see things like "virtual space/metaverse" in the future.
+        However, considering the inconvenience of VR/AR glasses, 
+        I am convinced that the future lies in short videos that can be viewed on the small screen of a smartphone.</p>
+      <p>The video "Thank You Mom" created by P&G for the Sochi Olympics got me thinking about this.
+        The video is only a little over two minutes long, but it is a wonderful piece of work 
+        that conveys the beauty of mothers and sports (the Olympics), and moves the viewer.</p>
+          <iframe class="mt-1 mb-1" width="320" height="180" src="https://www.youtube.com/embed/bQoJqDi8490" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <p>Of course, its primary objective of this video is the improvement of the brand image of P&G, 
+        but that is perfectly acceptable.</p>
+      <p>We felt that it would be wonderful if more and more of these wonderful works were made available to the public for free 
+        through Youtube and other means, and if this would inspire people and create a better world.</p>
+      <p>The idea was to organize a film festival to honor this type of work. As is appropriate in this day and age,
+         everything will be done online and open to anyone in the world, either as a creator or as a judge.</p>
+    </div>
+    <p class="text-3xl mb-2">この映画祭への思い</p>
+    <div class="space-y-2 mb-8">
       <p>映画祭としては、カンヌ映画祭やアカデミー賞が有名ですが、
         これらは昔ながらのビジネスモデルに縛られており、新しい形の映画祭が必要と感じているのです。</p>
       <p>情報を伝える手段は、基本的に、文章、文章＋画像、音声、音声＋画像、映像など、
@@ -18,7 +38,7 @@
         それで全然構わないのです。</p>
       <p>この手の素晴らしい作品が、より多く Youtube などを通して無料で公開され、それが結果として人々の心を動かし、
         より良い世の中が作られるのであれば、こんなに素晴らしいことはないと感じたのです。</p>
-      <p>そこで考えたのが、この手の作品を表彰する音楽祭の開催です。今の時代に相応しく、全てはオンラインで行い、
+      <p>そこで考えたのが、この手の作品を表彰する映画祭の開催です。今の時代に相応しく、全てはオンラインで行い、
         世界中の誰もが、クリエーターもしくは審査員として参加出来るようなオープンな仕組みです。</p>
     </div>
   </div>
