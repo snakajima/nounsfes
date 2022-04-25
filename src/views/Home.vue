7898192f138191fbd3633dda12819f93322cff54
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-04-25 14:20:47 -0700

    updated with indent

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,42 +7,44 @@
     </div>
 
     <p class="text-3xl mb-2">Nouns Art Festival</p>
-    <div class="space-y-2">
+    <div class="space-y-2 mb-8">
       <p>Nouns Art Festival (NounsFes) is an online art festival
-        with a purpose to promote a peaceful and sustainable world, founded by members of NounsDAO.</p>
+        with a purpose to promote a peaceful and sustainable world, 
+        founded by members of <a class="underline" href="https://nouns.wtf">NounsDAO</a>.</p>
       <p>We run the very first NounsFes in the form of 
         <b>an online art competition</b>, for the duration of six months in the following format. </p>
+    </div>
+    <div class="space-y-2">
       <ul>
-        <li><b>What:</b> A short video (a few minutes), 
+        <li class="ml-4 -indent-4"><b>What:</b> A short video (a few minutes), 
           which promotes and/or celebrates love, peace and sustainable earth (<a class="underline" href="https://sdgs.un.org/goals" target="_blank">SDGs</a>). </li>
-        <li><b>Who:</b> Any individuals, groups or entities (for-profit or non-profit) can submit</li>
-        <li><b>Why:</b> Inspire the audience and create a better future</li>
-        <li><b>How:</b> Upload the video to Youtube and submit its URL via the submission form 
+        <li class="ml-4 -indent-4"><b>Who:</b> Any individuals, groups or entities (for-profit or non-profit) can submit</li>
+        <li class="ml-4 -indent-4"><b>Why:</b> Inspire the audience and create a better future</li>
+        <li class="ml-4 -indent-4"><b>How:</b> Upload the video to Youtube and submit its URL via the submission form 
         (which will become available on June 1st below)</li>
         <li><b>When:</b>
-          <ul class="ml-4">
-            <li>Announcement: May 1st</li>
-            <li>Submission Period: June 1st - Sept 30th</li>
-            <li>Nomination: Oct 10th</li>
-            <li>Final: Oct 30th</li>
+          <ul class="ml-8">
+            <li class="list-disc">Announcement: May 1st</li>
+            <li class="list-disc">Submission Period: June 1st - Sept 30th</li>
+            <li class="list-disc">Nomination: Oct 10th</li>
+            <li class="list-disc">Final: Oct 30th</li>
           </ul>
         </li>
-        <li><b>Total prize:</b> To be announced</li>
-        <li><b>Sponsor:</b> NounsDAO, Nakajima Foundation and others (to be announced)</li>
+        <li class="ml-4 -indent-4"><b>Sponsors:</b> NounsDAO, Nakajima Foundation and others (to be announced)</li>
+        <li class="ml-4 -indent-4"><b>Prizes:</b> To be announced (mix of cash prizes and charitable contributions)</li>
         <li><b>Requirements:</b>
-          <ul class="ml-4">
-            <li>Please add on-second video clip of NounsFes (specified below) at the very beginning</li>
-            <li>Any format is fine (animation, 3d graphics, recorded, …)</li>
-            <li>You may or may not use Noun images and logos in the video.</li>
-            <li>Please add the link to NounsFes website in the description (of Youtube).</li>
+          <ul class="ml-8">
+            <li class="list-disc">Please add on-second video clip of NounsFes (specified below) at the very beginning</li>
+            <li class="list-disc">Any format is fine (animation, 3d graphics, recorded, …)</li>
+            <li class="list-disc">You may or may not use Noun images and logos in the video.</li>
+            <li class="list-disc">Please add the link to NounsFes website in the description (of Youtube).</li>
           </ul>
         </li>
-        <li><b>Judging process:</b> 
-        The judges will review the video themselves, 
-        but also their view counts, likes and comments and determine the winner.</li>
-        <li><b>Secondary Use:</b>
-        After the contest, we will use nominated videos to promote NounsFed and NounsDAO. 
-        We may create NFTs for nominated videos and share the proceeds. 
+        <li class="ml-4 -indent-4"><b>Judging Process:</b> 
+        The finalists and the winner will be determined based on their contents, view counts, likes and comments.</li>
+        <li class="ml-4 -indent-4"><b>Secondary Use:</b>
+        After the contest, we will use nominated videos to promote Nouns Art Festival. 
+        We <i>may</i> choose to create NFTs for the wining videos and finalists, and share the proceeds with creators and charitable organizations. 
         </li>
       </ul> 
     </div>
