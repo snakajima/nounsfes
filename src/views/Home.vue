4ecff77284c6f7c758c2173fdbe3873f6813b805
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-04-24 17:00:56 -0700

    Nouns Art Festival

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="home">
+  <div class="max-w-lg mx-auto text-left">
     <div class="flex justify-center items-center space-x-8">
       <!-- <img alt="Vue logo" src="../assets/logo.png" class="logo" /> -->
       <!-- Use Tailwind CSS h-40 (=10rem=160px) instead of .logo. -->
-      <img alt="Vue logo" src="../assets/logo.png" class="h-40" />
+      <!--img alt="Vue logo" src="../assets/logo.png" class="h-40" /-->
     </div>
 
-    <h1>Nouns Art Festival</h1>
-    <p>This is body</p>
+    <p class="text-3xl mb-2">Nouns Art Festival</p>
+    <div class="space-y-2">
+      <p>Nouns Art Festival (NounsFes) is an online art festival with a purpose to promote a peaceful and sustainable world, founded by members of NounsDAO.</p>
+      <p>We run the very first NounsFes in the form of an online art competition, for the duration of six months in the following format. </p>
+    </div>
   </div>
 </template>
 
