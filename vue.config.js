991aaabeebc064a8414d93c5ade208ce835d0b37
isamu <isamu@to-kyo.to>
2022-05-06 12:07:30 +0900

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.ProvidePlugin({
+             process: 'process/browser',
+      }),
+    ],
+    resolve: {
+      fallback: {
+        "buffer": require.resolve('buffer/'),
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+        "os": false,
+        "url": false,
+        "assert": false,
+      }
+    }
+  }
+};
