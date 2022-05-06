d889c00f1a537e1e605d10985ebdeef2d87eeb7b
isamu <isamu@to-kyo.to>
2022-05-06 10:23:43 +0900

    support web3

--- src/utils/MetaMask.ts
+++ src/utils/MetaMask.ts
@@ -1,14 +1,17 @@
+import Web3 from 'web3';
+
 export const ethereum = (window as any).ethereum;
-export const hasMetaMask =
-  typeof ethereum !== "undefined" && ethereum.isMetaMask;
+
+export const web3 = new Web3(Web3.givenProvider);
+
+export const hasMetaMask = Web3.givenProvider.isMetaMask; 
 
 export const requestAccount = async () => {
-  console.log(ethereum.request);
   const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-  return accounts.length > 0 ? accounts[0] : null;
+  return accounts.length > 0 ? accounts[0].toLowerCase() : null;
 };
 
 export const getAccount = async (): Promise<string | null> => {
-  const accounts = await ethereum.request({ method: "eth_accounts" });
-  return accounts.length > 0 ? accounts[0] : null;
+  const accounts = await web3.eth.requestAccounts();
+  return accounts.length > 0 ? accounts[0].toLowerCase() : null;
 };
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -72,7 +72,7 @@
 <script lang="ts">
 import { defineComponent, computed, ref } from "vue";
 import { useStore } from "vuex";
-import { hasMetaMask, requestAccount, ethereum } from "../utils/MetaMask";
+import { hasMetaMask, requestAccount, web3 } from "../utils/MetaMask";
 import { auth } from "../utils/firebase";
 import { signInWithCustomToken } from "firebase/auth";
 import { generateNonce, verifyNonce, deleteNonce } from "../utils/functions";
@@ -87,8 +87,10 @@ export default defineComponent({
       console.log("requestAccount")
       await requestAccount(); // ethereum.on('accountsChanged') will handle the result
     };
+
     const verifyIdentity = async () => {
       // Step 1: We get a nonce from the server
+
       isBusy.value = "Fetching a verification message from server...";
       const account = store.state.account;
       const result = await generateNonce({ account }) as any;
@@ -100,14 +102,12 @@ export default defineComponent({
       try {
         // Step 2: We ask the user to sign this nonce
         isBusy.value = "Waiting for you to sign a message...";
-        const signature = await ethereum.request({
-          method: "personal_sign",
-          params: [nonce, account],
-        });
+        const message = web3.utils.utf8ToHex(nonce);
+        
+        const signature = await web3.eth.personal.sign(nonce, account, "");
 
         // Step 3: We ask the server to verify the signature and get custom token
         const result2 = await verifyNonce({ signature, uuid }) as any;
-        console.log(result2.data);
         const token = result2.data.token;
         console.log("verifyIdentity: token", token);
         if (token) {
