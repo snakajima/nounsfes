2d3827c47594caef1f33192b7fb03d72588d6289
isamu <isamu@to-kyo.to>
2022-05-11 15:30:56 +0900

    update style

--- src/views/HomeEn.vue
+++ src/views/HomeEn.vue
@@ -14,15 +14,15 @@
       <p>We run the very first NounsFes in the form of 
         <b class="font-londrina">an online art competition</b>, for the duration of six months in the following format. </p>
     </div>
-    <div class="space-y-2">
+    <div class="space-y-2 font-pt-root">
       <ul>
-        <li class="ml-4 -indent-4"><b>What:</b> A short video (a few minutes), 
+        <li class="ml-4 -indent-4"><b class="font-londrina">What:</b> A short video (a few minutes), 
           which promotes and/or celebrates love, peace and sustainable earth (<a class="underline" href="https://sdgs.un.org/goals" target="_blank">SDGs</a>). </li>
-        <li class="ml-4 -indent-4"><b>Who:</b> Any individuals, groups or entities (for-profit or non-profit) can submit</li>
-        <li class="ml-4 -indent-4"><b>Why:</b> Inspire the audience and create a better future</li>
-        <li class="ml-4 -indent-4"><b>How:</b> Upload the video to Youtube and submit its URL via the submission form 
+        <li class="ml-4 -indent-4"><b class="font-londrina">Who:</b> Any individuals, groups or entities (for-profit or non-profit) can submit</li>
+        <li class="ml-4 -indent-4"><b class="font-londrina">Why:</b> Inspire the audience and create a better future</li>
+        <li class="ml-4 -indent-4"><b class="font-londrina">How:</b> Upload the video to Youtube and submit its URL via the submission form 
         (which will become available on June 1st below)</li>
-        <li><b>When:</b>
+        <li><b class="font-londrina">When:</b>
           <ul class="ml-8">
             <li class="list-disc">Announcement: May 1st, 2022</li>
             <li class="list-disc">Submission Period: June 1st - Sept 30th, 2022</li>
@@ -30,9 +30,9 @@
             <li class="list-disc">Final: Oct 30th, 2022</li>
           </ul>
         </li>
-        <li class="ml-4 -indent-4"><b>Sponsors:</b> NounsDAO, Nakajima Foundation and others (to be announced)</li>
-        <li class="ml-4 -indent-4"><b>Prizes:</b> 5eth + $25,000 (we are raising more from other sponsors)</li>
-        <li><b>Requirements:</b>
+        <li class="ml-4 -indent-4"><b class="font-londrina">Sponsors:</b> NounsDAO, Nakajima Foundation and others (to be announced)</li>
+        <li class="ml-4 -indent-4"><b class="font-londrina">Prizes:</b> 5eth + $25,000 (we are raising more from other sponsors)</li>
+        <li><b class="font-londrina">Requirements:</b>
           <ul class="ml-8">
             <li class="list-disc">Please add this one-second video clip at the very beginning (created by <a class="underline" href="https://twitter.com/Fuyu256S">Fuyu256</a> ). 
               <video width="320" height="240" controls>
@@ -44,13 +44,13 @@
             <li class="list-disc">Please add the link to NounsFes website (https://nounsfes.org) in the video's description.</li>
           </ul>
         </li>
-        <li class="ml-4 -indent-4"><b>Judging Process:</b> 
+        <li class="ml-4 -indent-4"><b class="font-londrina">Judging Process:</b> 
         The finalists and the winner will be determined based on their contents, view counts, likes and comments.</li>
-        <li class="ml-4 -indent-4"><b>Secondary Use:</b>
+        <li class="ml-4 -indent-4"><b class="font-londrina">Secondary Use:</b>
         After the contest, we will use nominated videos to promote Nouns Art Festival. 
         We <i>may</i> choose to create NFTs for the wining videos and finalists, and share the proceeds with creators and charitable organizations. 
         </li>
-        <li class="ml-4 -indent-4"><b>Inspiring Video Examples:</b> 
+        <li class="ml-4 -indent-4"><b class="font-londrina">Inspiring Video Examples:</b> 
           <iframe class="mt-1 mb-1" width="320" height="180" src="https://www.youtube.com/embed/bQoJqDi8490" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           <iframe class="mb-1" width="320" height="180" src="https://www.youtube.com/embed/WmVLcj-XKnM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           <iframe class="mb-1" width="320" height="180" src="https://www.youtube.com/embed/v27CEFE02Hs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
