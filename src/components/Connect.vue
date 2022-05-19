0bba2d4efed9da62f5dbf1351fe8b5092f3984d3
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-05-19 16:24:42 -0700

    button CSS

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -2,14 +2,14 @@
   <span class="ml-16 font-londrina font-yusei">
     <span v-if="hasMetaMask">
       <span v-if="account">
-          <button>{{ $t("menu.connected") }}</button>
+          <button class="inline-block px-6 py-2.5 bg-green-700 text-white leading-tight rounded shadow-md">{{ $t("menu.connected") }}</button>
       </span>
       <span v-else>
-          <button @click="connect" class="rounded-xl">{{ $t("menu.connect") }}</button>
+          <button @click="connect" class="inline-block px-6 py-2.5 bg-green-500 text-white leading-tight rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out">{{ $t("menu.connect") }}</button>
       </span>
     </span>
     <span v-else>
-      <button>{{ $t("menu.nometamask") }}</button>
+      <button class="inline-block px-6 py-2.5 bg-red-600 text-white leading-tight rounded shadow-md">{{ $t("menu.nometamask") }}</button>
     </span>
   </span>
 </template>
