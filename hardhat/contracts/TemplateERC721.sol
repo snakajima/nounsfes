6a9b941fd12036acd90e9f3ccde6c9530ff4a6ce
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-05-24 14:13:35 -0700

    abstract

--- hardhat/contracts/TemplateERC721.sol
+++ hardhat/contracts/TemplateERC721.sol
@@ -3,6 +3,5 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract TemplateERC721 is ERC721URIStorage {
-    constructor() ERC721("Template", "item") {}
+interface TemplateERC721 is IERC721 {
 }
