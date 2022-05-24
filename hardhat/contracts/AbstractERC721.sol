9ea97e80d3171506895683337eec410df952a457
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-05-24 14:28:57 -0700

    clean up

--- hardhat/contracts/AbstractERC721.sol
+++ hardhat/contracts/AbstractERC721.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
 interface AbstractERC721 is IERC721 {
 }
