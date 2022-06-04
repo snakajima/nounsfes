c77a47202f4db285b2d82732db174c94fe51e457
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-06-03 21:09:37 -0700

    import store for utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,11 @@
 import { computed } from "vue";
-import { useStore } from "vuex";
+import store from "../store";
 
 export const useUser = () => {
-  const store = useStore();
   const user = computed(() => store.state.user);
   return user;
 };
 export const useIsSignedIn = () => {
-  const store = useStore();
   const isSignedIn = computed(() => store.getters.isSignedIn);
   return isSignedIn;
 };
