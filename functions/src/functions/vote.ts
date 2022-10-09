2ccae72f68d7405de414ff8976dd1b434b2b7a5e
TAK_Ryuji <kozayupapa@gmail.com>
2022-10-09 15:56:33 +0900

    add vote function

---
+++ functions/src/functions/vote.ts
@@ -0,0 +1,83 @@
+import * as admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+const firestore = admin.firestore();
+
+//export const vote_events = functions.https.onRequest(express.app);
+
+export const vote = async (data:any, context:any) => {
+  const {voteEventId, selectionId,uid} = data;
+  if (!context.auth || !context.auth.uid) {
+    return {
+        retult: false,
+        message: "no auth"
+      };
+    }
+  if (context.auth.uid != uid){
+    console.log(`param uid:${uid} context:${context.auth.uid}`)
+    return {
+        retult: false,
+        message: "invalid uid"
+      };
+  }
+
+  const vote_eventRef = await firestore.doc(`/vote_events/${voteEventId}`).get();
+  console.log(vote_eventRef);
+  //don't create selection doc for 2022 fes contest 
+  //const selectionRef = await firestore.doc(`/vote_events/${voteEventId}/votes/${selectionId}`).get();
+
+  // check vote_event, and answer.
+  if (!vote_eventRef.exists) {
+    return {
+      retult: false,
+      message: "no data"
+    };
+  }
+
+  
+  const voteLogRef = await firestore.doc(`/users/${uid}/private/votes`).get();
+  
+  const voteLog = voteLogRef.data() || {voted: {}};
+  
+  if (!voteLog.voted) {
+    return {
+      retult: false,
+      message: "invalid data"
+    };
+  }
+
+  // check user's vote not yet
+  if (voteLog.voted[voteEventId]) {
+    return {
+      retult: false,
+      message: "voted"
+    };
+    
+  }
+
+  // update summary
+
+  // todo move to create vote_event;
+  await admin.firestore().runTransaction(async (tr) => {
+    const path = `/vote_events/${voteEventId}/results/${selectionId}`;
+    const vote_eventSummaryRef = await tr.get(firestore.doc(path));
+    if (!vote_eventSummaryRef.exists) {
+      await tr.set(firestore.doc(path), {});
+    }
+
+    await tr.update(firestore.doc(path),
+                    {counter: admin.firestore.FieldValue.increment(1)});
+  
+    // add log
+    voteLog.voted[voteEventId] = true;
+
+    await tr.set(firestore.doc(`/users/${uid}/private/votes`), voteLog);
+  });
+
+  return {
+    retult: true
+  };
+  
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,3 +18,5 @@ exportIfNeeded("test", "tests/test", exports);
 exportIfNeeded("verifyNonce", "nonces/verifyNonce", exports);
 exportIfNeeded("generateNonce", "nonces/generateNonce", exports);
 exportIfNeeded("deleteNonce", "nonces/deleteNonce", exports);
+exportIfNeeded("vote", "vote/vote", exports);
+
